5c16104845e63cdcb7bcfd05d31690f590c8b7a9
Kevin Li <kevinli020508@gmail.com>
2020-01-23 00:30:33 -0500

    Add RandomBox

---
+++ src/Noise/RandomBox.elm
@@ -0,0 +1,85 @@
+module Noise.RandomBox exposing (..)
+
+import Browser
+import Color
+import Html exposing (Html)
+import TypedSvg as Svg
+import TypedSvg.Core exposing (Svg)
+import TypedSvg.Attributes as Attributes
+import TypedSvg.Types exposing (Fill(..), px)
+import Random
+
+type alias Model =
+  { shades : List Float
+  }
+
+type Msg
+  = GetShades (List Float)
+
+width : Float
+width =
+  100
+
+height : Float
+height =
+  100
+
+main : Program () Model Msg
+main =
+  Browser.element
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ( { shades = []
+  }
+  , Random.generate GetShades <| Random.list (round <| width * height) (Random.float 0 1)
+  )
+  
+
+view : Model -> Html msg
+view model =
+  Svg.svg
+    [ Attributes.width (px 600)
+    , Attributes.height (px 600)
+    , Attributes.viewBox 0 0 600 600
+    ] <|
+    List.indexedMap
+      point
+      model.shades
+
+
+point : Int -> Float -> Svg msg
+point position shade =
+  let
+    x =
+      toFloat <| modBy (round width) position
+    y =
+      toFloat <| round <| toFloat position / width
+  in
+  Svg.circle
+    [ Attributes.cx (px x)
+    , Attributes.cy (px y)
+    , Attributes.r (px 1)
+    , Attributes.fill <| Fill <| Color.rgb shade shade shade
+    ]
+    []
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  ( case msg of
+    GetShades shades ->
+      { model |
+        shades = shades
+      }
+  , Cmd.none
+  )
